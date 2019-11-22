1fe73880ab6800c84a8924427240b76ec501f442
yangweixing <yangweixing@szhg9.com>
2019-11-22 17:46:04 +0800

    update

--- pages/time/time.js
+++ pages/time/time.js
@@ -4,33 +4,6 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     hasTask: false,
-    list: [
-      {
-        name: '工作',
-        value: 0,
-        checked: false
-      },
-      {
-        name: '健身',
-        value: 1,
-        checked: false
-      },
-      {
-        name: '通勤',
-        value: 2,
-        checked: false
-      },
-      {
-        name: '阅读',
-        value: 3,
-        checked: false
-      },
-      {
-        name: '学习',
-        value: 4,
-        checked: false
-      }
-    ]
   },
   switchTime: function (e) {
     console.log(e)
@@ -41,7 +14,7 @@ Page({
       })
       return
     }
-    if (! this.data.hasUserInfo){
+    if (!this.data.hasUserInfo){
       canIUse: wx.canIUse('button.open-type.getUserInfo')
     }
     let index = e.currentTarget.dataset.index
@@ -52,7 +25,9 @@ Page({
       if (i === index) {
         list[i].checked = checked
       } else {
-        list[i].checked = !checked
+        if(checked){
+          list[i].checked = !checked
+        }
       }
     }
     this.setData({
@@ -63,6 +38,34 @@ Page({
     
   },
   onShow() {
-    
+    this.setData({
+      list: [
+        {
+          name: '工作',
+          value: 0,
+          checked: false
+        },
+        {
+          name: '健身',
+          value: 1,
+          checked: false
+        },
+        {
+          name: '通勤',
+          value: 2,
+          checked: false
+        },
+        {
+          name: '阅读',
+          value: 3,
+          checked: false
+        },
+        {
+          name: '学习',
+          value: 4,
+          checked: false
+        }
+      ]
+    })
   }
 })
